81d702a5e24be26c84f7456e9915145962583039
nagitak <nagitak>
2022-02-12 21:02:47 +0900

    date 부분 삭제

--- routes/posting.js
+++ routes/posting.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const router = express.Router();
-// const authmiddlewares = require('../middlewares/auth-middleware');
+const authmiddlewares = require('../middlewares/auth-middleware');
 
 const posting = require('../schemas/postSchema');
 
@@ -9,18 +9,14 @@ router.use(bodyParser.urlencoded({ extended: false }));
 router.use(bodyParser.json());
 
 // 게시글 작성 //
-// 유저 닉네임도 저장한다?
 
 router.post('/items', authmiddlewares, async (req, res) => {
-  const { item_url, title, price, description, date } = req.body;
-  // const { user } = res.locals;
+  const { item_url, title, price, description } = req.body;
   await posting.create({
-    /*name: user['nickname'], */
     item_url,
     title,
     price,
     description,
-    date,
   });
   res.send({});
 });
@@ -28,15 +24,14 @@ router.post('/items', authmiddlewares, async (req, res) => {
 // 전체 게시글 조회 //
 
 router.get('/items', async (req, res) => {
-  const postings = await posting.find().sort({ date: -1 });
+  const postings = await posting.find();
   res.json({ list: postings });
 });
 
-// detail 화면에 보일 게시글 + 댓글 조회
+// detail 화면에 보일 게시글 조회
 
 router.get('/detail/:itemid', async (req, res) => {
-  const postings = await postings.findById(req.params.itemid);
-  const comments = await comment.find({ posting_url: id });
+  const postings = await posting.findById(req.params.itemid);
 
   res.json({ list: postings, comments });
 });
@@ -44,21 +39,21 @@ router.get('/detail/:itemid', async (req, res) => {
 // 수정 페이지 접속
 
 router.get('/items/:itemid', async (req, res) => {
-  const postings = await postings.findById(req.params.itemid);
+  const postings = await posting.findById(req.params.itemid);
   res.json({ list: postings });
 });
 
 // 수정 페이지 접속 후 삭제
 
 router.post('/items/:itemid/delete', async (req, res) => {
-  await postings.deleteOne({ _id: req.params.itemid });
+  await posting.deleteOne({ _id: req.params.itemid });
   res.json({ message: '삭제가 완료됐습니다.' });
 });
 
 // 수정 페이지 접속 후 수정
 
 router.post('/items/:itemid/modify', async (req, res) => {
-  const { item_url, title, price, description, date } = req.body;
+  const { item_url, title, price, description } = req.body;
   await posting
     .findByIdAndUpdate(req.params.itemid, {
       $set: {
@@ -66,7 +61,6 @@ router.post('/items/:itemid/modify', async (req, res) => {
         title: title,
         price: price,
         description: description,
-        date: date,
       },
     })
     .exec();
