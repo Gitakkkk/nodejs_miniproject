92a9d7e5796a5c69638f094b34ff300837760262
nagitak <nagitak>
2022-02-15 10:22:04 +0900

    token 뽑아내기

--- schemas/user.js
+++ schemas/user.js
@@ -1,13 +1,39 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
+// const bcrypt = require('bcrypt');
+// const saltRound = 10;
 
 const UserSchema = new mongoose.Schema({
   nickname: String,
   password: String,
 });
-UserSchema.virtual("userId").get(function () {
+UserSchema.virtual('userId').get(function () {
   return this._id.toHexString();
 });
-UserSchema.set("toJSON", {
+UserSchema.set('toJSON', {
   virtuals: true,
 });
-module.exports = mongoose.model("User", UserSchema);
+
+// UserSchema.pre('save', function (next) {
+//   var user = this;
+//   if (user.isModified('password')) {
+//     bcrypt.genSalt(saltRound, function (err, salt) {
+//       if (err) return next(err);
+
+//       bcrypt.hash(user.password, salt, function (err, hash) {
+//         if (err) return next(err);
+//         user.password = hash;
+//         next();
+//       });
+//     });
+//   } else {
+//     next();
+//   }
+// });
+
+// UserSchema.methods.comparePassword = function (plainPassword, cb) {
+//   bcrypt.compare(plainPassword, this.password, function (err, isMatch) {
+//     if (err) return cb(err), cb(null, isMatch);
+//   });
+// };
+
+module.exports = mongoose.model('User', UserSchema);
