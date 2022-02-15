f3b5fd9656b4271c9b0a06712116fbdc3971f268
nagitak <nagitak>
2022-02-15 19:25:46 +0900

    오류 수정

--- middlewares/auth-middleware.js
+++ middlewares/auth-middleware.js
@@ -1,29 +1,30 @@
 const jwt = require('jsonwebtoken');
-const User = require('../schemas/user')
+const User = require('../schemas/user');
 
 module.exports = (req, res, next) => {
-    const { authorization } = req.headers;
-    const [tokenTpye, tokenValue] = authorization.split(' ');
+  const { authorization } = req.headers;
+  const [tokenTpye, tokenValue] = authorization.split(' ');
 
-    if (tokenTpye !== 'Bearer') {
-        res.status(401).send({
-            errorMessage: '로그인 후 사용하세요'
-        });
-        return;
-    }
+  if (tokenTpye !== 'Bearer') {
+    res.status(401).send({
+      errorMessage: '1',
+    });
+    return;
+  }
 
-    try {
-        const { nickname } = jwt.verify(tokenValue, `${process.env.SECRET_KEY}`);
+  try {
+    const { nickname } = jwt.verify(tokenValue, `${process.env.SECRET_KEY}`);
 
-        const user = User.find({ nickname }).exec().then((user) => {
-            res.locals.user = user;
-            next();
-        });
-
-    } catch (error) {
-        res.status(401).send({
-            errorMessage: '로그인 후 사용하세요'
-        });
-        return;
-    }
+    const user = User.find({ nickname })
+      .exec()
+      .then((user) => {
+        res.locals.user = user;
+        next();
+      });
+  } catch (error) {
+    res.status(401).send({
+      errorMessage: '2',
+    });
+    return;
+  }
 };
